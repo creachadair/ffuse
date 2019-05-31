96c986db613e32869c64a5b14cedea2e7336a2be
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-05-31 15:19:40 -0700

    Add comments to the platform-specific files.

--- xattr_darwin.go
+++ xattr_darwin.go
@@ -6,9 +6,12 @@ import (
 	"bazil.org/fuse"
 )
 
+// Symbolic constants for extended attributes, macOS version.
+// These are not exposed in the syscall package.
 const (
 	xattrCreate  = 2 // XATTR_CREATE for setxattr(2)
 	xattrReplace = 4 // XATTR_REPLACE for setxattr(2)
 
+	// The errno returned for "xattr not found".
 	xattrErrnoNotFound = fuse.Errno(syscall.ENOATTR)
 )
--- xattr_linux.go
+++ xattr_linux.go
@@ -6,9 +6,12 @@ import (
 	"bazil.org/fuse"
 )
 
+// Symbolic constants for extended attributes, Linux version.
+// These are not exposed in the syscall package.
 const (
 	xattrCreate  = 1 // XATTR_CREATE for setxattr(2)
 	xattrReplace = 2 // XATTR_REPLACE for setxattr(2)
 
+	// The errno returned for "xattr not found".
 	xattrErrnoNotFound = fuse.Errno(syscall.ENODATA)
 )
